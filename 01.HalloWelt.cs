9cc054d6a46d569ae908ff62dd5c5c824c869e22
Nevena <nevena.rogova@gmx.at>
2025-04-03 20:35:40 +0300

    Add files via upload

---
+++ 01.HalloWelt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BSP1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("============");
+            Console.WriteLine();
+
+            Console.WriteLine("Bitte druecken Sie die ENTER-Taste");
+            Console.ReadLine();
+
+        }
+    }
+}
