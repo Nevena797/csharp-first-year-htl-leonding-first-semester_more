b28e59e1d2a984e1cc116cff1b5461964dee778f
Nevena <nevena.rogova@gmx.at>
2025-04-03 20:38:25 +0300

    Add files via upload

---
+++ 04.BSP06größteZahl1.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.BSP06größteZahl1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 0, numberTwo = 0, numberThree = 0, bigestNumber = 0;
+
+            Console.Write("Geben Sie bitte den 1. Wert ein: ");
+            numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geben Sie bitte den 2. Wert ein: ");
+            numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geben Sie bitte den 3. Wert ein: ");
+            numberThree = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOne < numberTwo)
+            {
+                if (numberTwo < numberThree)
+                {
+                    bigestNumber = numberThree;
+                }
+                else
+                {
+                    bigestNumber = numberTwo;
+                }
+            }
+            else 
+            {
+                bigestNumber = numberOne;
+            }
+
+            Console.WriteLine($"Größte Zahl ist: {bigestNumber}");
+        }
+    }
+}
